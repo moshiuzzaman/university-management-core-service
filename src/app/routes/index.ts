24c025ded74f335f1eb2b1552d3bf29759ba188e
Shozon <moshi.bs23@gmail.com>
2023-08-27 23:26:54 +0600

    feat(server): academic semester module added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
+import { AcademicSemeterRoutes } from '../modules/academicSemester/academicSemester.routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
   // ... routes
   {
-    path: "",
-    routes: ""
+    path: "/academic-semesters",
+    route: AcademicSemeterRoutes
   }
 ];
 
